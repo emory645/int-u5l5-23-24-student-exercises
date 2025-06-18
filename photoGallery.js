dd64a4a2d9980f0dd3f9f643230ba4c54b84a2cf
emory645 <emorypp11@gmail.com>
2025-06-18 12:56:18 -0500

    5.5

--- photoGallery.js
+++ photoGallery.js
@@ -4,10 +4,13 @@
 
 
 // 2. Add at least four image urls to the imageUrls array
-let imageUrls = [];
+let imageUrls = ["https://www.google.com/imgres?q=random%20image&imgurl=https%3A%2F%2Fi.redd.it%2Fi-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png%3Fwidth%3D1280%26format%3Dpng%26auto%3Dwebp%26s%3D7177756d1f393b6e093596d06e1ba539f723264b&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2FPixelArt%2Fcomments%2F15z47dt%2Fi_got_bored_so_i_decided_to_draw_a_random_image%2F&docid=8oeNXf9H_px_AM&tbnid=WS5PkqrbWh_neM&vet=12ahUKEwj90ubFxPuNAxUDM1kFHZwsFvwQM3oECGQQAA..i&w=1280&h=1280&hcb=2&ved=2ahUKEwj90ubFxPuNAxUDM1kFHZwsFvwQM3oECGQQAA",
+    "https://www.google.com/imgres?q=random%20image&imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2021%2F12%2F12%2F20%2F00%2Fplay-6865967_640.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Frandom%2520object%2F&docid=y0kpRIiESjMhuM&tbnid=VUUIkjB3tQbZpM&vet=12ahUKEwj90ubFxPuNAxUDM1kFHZwsFvwQM3oECCQQAA..i&w=640&h=427&hcb=2&ved=2ahUKEwj90ubFxPuNAxUDM1kFHZwsFvwQM3oECCQQAA",
+
+];
 
 // 3. Selecting the imageUrls array, create a forEach loop.
-
+imageUrls.forEach
 
 // In side the body of the loop:
 //  - Create an image element and store it in a variable named 'image'
